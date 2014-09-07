ed683b0b6bc683c65510b1bf3d526d087072531f
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-09-06 19:14:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InsectProtein::Application.config.secret_key_base = '1e29a5abc22789285377aa6919f1bb079668e2606c3f6fd64e070587eaf2b2769648eeb9044eec585920cd9d59f7ed8efc32e2a74ae1d57c31aae8b0bfe51990'
